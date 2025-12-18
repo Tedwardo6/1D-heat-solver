79f651078230288d45adc7adabfae188be9d684a
Tedwardo6 <theodoreveteto@gmail.com>
2025-12-18 15:18:44 -0800

    animation test

--- animation.py
+++ animation.py
@@ -3,11 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-
 # initial_behav = math.sin(dx*t)
-L = 20  #length 
+L = 20  #length
 dx = 1  # distance between x points
-dt = 1 # time between steps 
+dt = 1  # time between steps
 alpha = 0.1 # diffusion constant
 xi = 0
 xf = L
@@ -24,7 +23,6 @@ x_i = [0] * K
 
 color_test = (1, 0, 0)
 
-
 a = (alpha*dt)/(dx**2)
 
 # set initial condition
@@ -36,10 +34,18 @@ for i in range(xi, K, 1):
 u_left = np.roll(u, 1)
 u_right = np.roll(u, -1)
 
-plt.plot(x_i,u, color='blue', label=f"t = {ti}")
+fig, ax = plt.subplots()
+line, = ax.plot(x_i, u, color='red', label=f"t = {ti}")
 
-# compare values
-for j in range(0,N,1):
+ax.set_xlabel("x")
+ax.set_ylabel("u")
+ax.legend()
+ax.set_xlim(x_i[0], x_i[-1])
+ax.set_ylim(-1.2, 1.2)
+
+def update(frame):
+    global u, u_next, u_left, u_right, color_test
+
     for i in range(0, K, 1):
         diff_left = u[i] - u_left[i]
         diff_right = u[i] - u_right[i]
@@ -47,16 +53,22 @@ for j in range(0,N,1):
         # u_left and u_right should decrease based on their contributions, but this should be done when u_left and right are treated as u
         surrounding = diff_left + diff_right
         u_next[i] = u[i] - (surrounding * a)
-        #plt.plot(x_i,u, color = color_test)
-        #color_test = (1,0,(i*(1/K)))
-        print(j,i,max(u),min(u))
-    u = u_next 
+
+        # printing inside an animation will spam + slow it down; uncomment if you really want it
+        # print(frame, i, float(u.max()), float(u.min()))
+
+    u = u_next
     u_next = [0] * K
     u_left = np.roll(u, 1)
     u_right = np.roll(u, -1)
 
-plt.plot(x_i,u, color='red',label=f"t = {tf}")
-plt.xlabel("x")
-plt.ylabel("u")
-plt.legend()
+    line.set_ydata(u)
+    line.set_color(color_test)
+    fraction = frame / (N - 1)
+    color_test = (1 - fraction, 0, fraction)
+    ax.set_title(f"t = {ti + frame*dt}")
+    return (line,)
+
+anim = FuncAnimation(fig, update, frames=N, interval=50, blit=False, repeat=False)
+
 plt.show()
